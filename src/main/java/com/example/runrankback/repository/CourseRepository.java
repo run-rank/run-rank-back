44fdc4f82135893e6016a756f37318f95362a990
mijeong09 <me9809180@gmail.com>
2026-02-10 14:39:57 +0900

    feat: 주변 코스 검색 쿼리 추가
    특정 범위 내에 있는 코스 목록을 출발지의 위도/경도 기준으로 검색

--- src/main/java/com/example/runrankback/repository/CourseRepository.java
+++ src/main/java/com/example/runrankback/repository/CourseRepository.java
@@ -3,5 +3,10 @@ package com.example.runrankback.repository;
 import com.example.runrankback.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByStartLatBetweenAndStartLngBetween(
+            Double minLat, Double maxLat, Double minLag, Double maxLag
+    );
 }
