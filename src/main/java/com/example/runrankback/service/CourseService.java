163c6d05a53a9a14f09273f98960977921554084
mijeong09 <me9809180@gmail.com>
2026-02-20 13:14:14 +0900

    feat: 코스 데이터 변환 로직 추가
    - 좌표 데이터를 LineString 객체로 변환

--- src/main/java/com/example/runrankback/service/CourseService.java
+++ src/main/java/com/example/runrankback/service/CourseService.java
@@ -5,14 +5,12 @@ import com.example.runrankback.dto.response.CourseResponseDto;
 import com.example.runrankback.entity.Course;
 import com.example.runrankback.entity.User;
 import com.example.runrankback.repository.CourseRepository;
-import com.example.runrankback.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.PrecisionModel;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -60,18 +58,23 @@ public class CourseService {
 
         try{
             Coordinate[] coordinates = route.stream()
-                    .map(pointMap -> {
-                        double lat = ((Number) pointMap.get("lat")).doubleValue();
-                        double lng = ((Number) pointMap.get("lng")).doubleValue();
-
-                        return new Coordinate(lng, lat);
-                    })
+                    .map(point -> new Coordinate(
+                            Double.parseDouble(point.get("lng").toString()),
+                            Double.parseDouble(point.get("lat").toString())
+                    ))
                     .toArray(Coordinate[]::new);
 
-            return geometryFactory.createLinearRing(coordinates);
+            return geometryFactory.createLineString(coordinates);
         }
         catch(Exception e) {
             throw new IllegalArgumentException("올바르지 않은 경로 데이터 형식: " + e.getMessage());
         }
+    }
+
+    public CourseResponseDto getCourseDetail(Long courseId) {
+        Course course = courseRepository.findById(courseId)
+                .orElseThrow(() -> new EntityNotFoundException("코스를 찾을 수 없습니다."));
+
+        return CourseResponseDto.from(course);
     }
 }
